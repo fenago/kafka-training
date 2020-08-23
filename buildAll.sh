5dc0028ecf1539ac2cfa4b508b6827f6d638c7f9
Ernesto Lee <gei411@gmail.com>
2020-08-23 20:31:52 +0000

    kafka labs added

---
+++ buildAll.sh
@@ -0,0 +1,292 @@
+#!/usr/bin/env bash
+
+export PROJECT_DIR=`pwd`
+
+
+cd lab3
+mvn clean install
+cd $PROJECT_DIR
+cd lab3/solution
+mvn clean install
+cd $PROJECT_DIR
+
+#
+#include 'lab2/solution'
+#//include 'lab2' does not compile until they fix build
+#
+
+
+cd lab2/solution
+mvn clean install
+cd $PROJECT_DIR
+
+
+cd lab5.1
+mvn clean install
+cd $PROJECT_DIR
+cd lab5.1/solution
+mvn clean install
+cd $PROJECT_DIR
+
+cd lab5.2
+mvn clean install
+cd $PROJECT_DIR
+cd lab5.2/solution
+mvn clean install
+cd $PROJECT_DIR
+
+
+cd lab5.3
+mvn clean install
+cd $PROJECT_DIR
+cd lab5.3/solution
+mvn clean install
+cd $PROJECT_DIR
+
+
+cd lab5.4
+mvn clean install
+cd $PROJECT_DIR
+cd lab5.4/solution
+mvn clean install
+cd $PROJECT_DIR
+
+cd lab5.5
+mvn clean install
+cd $PROJECT_DIR
+cd lab5.5/solution
+mvn clean install
+cd $PROJECT_DIR
+
+#
+#include 'lab5.6/solution'
+#include 'lab5.6'
+#
+
+
+cd lab5.6
+mvn clean install
+cd $PROJECT_DIR
+cd lab5.6/solution
+mvn clean install
+cd $PROJECT_DIR
+
+#include 'lab5.7/solution'
+#include 'lab5.7'
+
+
+cd lab5.7
+mvn clean install
+cd $PROJECT_DIR
+cd lab5.7/solution
+mvn clean install
+cd $PROJECT_DIR
+#
+#include 'lab5.8/solution'
+#include 'lab5.8'
+#
+
+cd lab5.8
+mvn clean install
+cd $PROJECT_DIR
+cd lab5.8/solution
+mvn clean install
+cd $PROJECT_DIR
+
+#include 'lab5.PRIME/solution'
+#include 'lab5.PRIME'
+#
+
+cd lab5.PRIME
+mvn clean install
+cd $PROJECT_DIR
+cd lab5.PRIME/solution
+mvn clean install
+cd $PROJECT_DIR
+
+#include 'lab6.1/solution'
+#include 'lab6.1'
+#
+cd lab6.1
+mvn clean install
+cd $PROJECT_DIR
+cd lab6.1/solution
+mvn clean install
+cd $PROJECT_DIR
+
+#include 'lab6.2/solution'
+#include 'lab6.2'
+#
+cd lab6.2
+mvn clean install
+cd $PROJECT_DIR
+cd lab6.2/solution
+mvn clean install
+cd $PROJECT_DIR
+
+
+#include 'lab6.3/solution'
+#include 'lab6.3'
+#
+cd lab6.3
+mvn clean install
+cd $PROJECT_DIR
+cd lab6.3/solution
+mvn clean install
+cd $PROJECT_DIR
+
+
+#include 'lab6.4/solution'
+#include 'lab6.4'
+#
+cd lab6.4
+mvn clean install
+cd $PROJECT_DIR
+cd lab6.4/solution
+mvn clean install
+cd $PROJECT_DIR
+
+#include 'lab6.5/solution'
+#include 'lab6.5'
+#
+cd lab6.5
+mvn clean install
+cd $PROJECT_DIR
+cd lab6.5/solution
+mvn clean install
+cd $PROJECT_DIR
+
+
+#include 'lab6.6/solution'
+#include 'lab6.6'
+#
+cd lab6.6
+mvn clean install
+cd $PROJECT_DIR
+cd lab6.6/solution
+mvn clean install
+cd $PROJECT_DIR
+
+
+#include 'lab6.7/solution'
+#include 'lab6.7'
+#
+cd lab6.7
+mvn clean install
+cd $PROJECT_DIR
+cd lab6.7/solution
+mvn clean install
+cd $PROJECT_DIR
+
+
+#include 'lab6.PRIME/solution'
+#include 'lab6.PRIME'
+#
+cd lab6.PRIME
+mvn clean install
+cd $PROJECT_DIR
+cd lab6.PRIME/solution
+mvn clean install
+cd $PROJECT_DIR
+
+
+#include 'lab7.1/solution'
+#//include 'lab7.1' Does not work until they fix
+#
+cd lab7.1/solution
+mvn clean install
+cd $PROJECT_DIR
+
+#include 'lab7.2/solution'
+#include 'lab7.2'
+#
+cd lab7.2
+mvn clean install
+cd $PROJECT_DIR
+cd lab7.2/solution
+mvn clean install
+cd $PROJECT_DIR
+
+
+#
+#include 'lab8.1/solution'
+#include 'lab8.1'
+#
+cd lab8.1
+mvn clean install
+cd $PROJECT_DIR
+cd lab8.1/solution
+mvn clean install
+cd $PROJECT_DIR
+
+
+#include 'lab8.2/solution'
+#include 'lab8.2'
+#
+cd lab8.2
+mvn clean install
+cd $PROJECT_DIR
+cd lab8.2/solution
+mvn clean install
+cd $PROJECT_DIR
+
+
+#include 'lab8.3/solution'
+#include 'lab8.3'
+#
+cd lab8.3
+mvn clean install
+cd $PROJECT_DIR
+cd lab8.3/solution
+mvn clean install
+cd $PROJECT_DIR
+
+
+#include 'lab8.4/solution'
+#include 'lab8.4'
+#
+cd lab8.4
+mvn clean install
+cd $PROJECT_DIR
+cd lab8.4/solution
+mvn clean install
+cd $PROJECT_DIR
+
+
+#include 'lab9/solution'
+#include 'lab9'
+#
+cd lab9
+mvn clean install
+cd $PROJECT_DIR
+cd lab9/solution
+mvn clean install
+cd $PROJECT_DIR
+
+
+#include 'lab10/solution'
+#include 'lab10'
+#
+cd lab10
+mvn clean install
+cd $PROJECT_DIR
+cd lab10/solution
+mvn clean install
+cd $PROJECT_DIR
+
+
+#include 'schema-registry'
+#
+cd schema-registry
+mvn clean install
+cd $PROJECT_DIR
+
+
+#include 'stream-lab1-2/solution'
+#include 'stream-lab1-2'
+cd stream-lab1-2
+mvn clean install
+cd $PROJECT_DIR
+cd stream-lab1-2/solution
+mvn clean install
+cd $PROJECT_DIR
---
+++ run-kafka.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+cd ~/kafka-training
+
+kafka/bin/kafka-server-start.sh \
+    kafka/config/server.properties
+
+
