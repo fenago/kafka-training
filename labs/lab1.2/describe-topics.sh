1cf4a7a3c020d371b745977f670f6f089f4433b1
fenago <socrates73@gmail.com>
2021-05-25 16:29:46 -0400

    Update describe-topics.sh

--- labs/lab1.2/describe-topics.sh
+++ labs/lab1.2/describe-topics.sh
@@ -3,7 +3,7 @@
 cd ~/kafka-training
 
 #TODO DESCRIBE my-failsafe-topic
-# HINT: kafka/bin/kafka-topics.sh --describe \
+kafka/bin/kafka-topics.sh --describe --topic my-failsafe-topic --zookeeper localhost:2181
 # TODO PASS NAME OF THE TOPIC
 # HINT    --topic my-failsafe-topic \
 # TODO PASS ZOOKEEPER ADDRESS
