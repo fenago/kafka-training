3b6346a73ac1796258f67b97611a9fd3dbdb2cdc
fenago <socrates73@gmail.com>
2021-05-25 15:41:27 -0400

    Update start-producer-console.sh

--- labs/lab1/start-producer-console.sh
+++ labs/lab1/start-producer-console.sh
@@ -3,7 +3,7 @@ cd ~/kafka-training
 
 
 ## TODO Run the producer console.
-# HINT: kafka/bin/kafka-console-producer.sh
+kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic my-topic
 ## TODO pass the borker list. Recall the broker runs on port 9092.
 # HINT: --broker-list localhost:9092 \
 ## TODO Pass the topic that we just created.
