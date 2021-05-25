cd49281436a279b4a47d430177215d7b13291000
fenago <socrates73@gmail.com>
2021-05-25 15:39:24 -0400

    Update list-topics.sh

--- labs/lab1/list-topics.sh
+++ labs/lab1/list-topics.sh
@@ -5,9 +5,6 @@ cd ~/kafka-training
 # TODO List existing topics
 # TODO Pass the parameter --list.
 # TODO pass the zookeeper address
-kafka/bin/kafka-topics.sh
-
-#HINT --list \
-#    --zookeeper localhost:2181
+kafka/bin/kafka-topics.sh --list --zookeeper localhost:2181
 
 
