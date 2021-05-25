6e52a5718922991efdf8bb8bb487670931ff440c
fenago <socrates73@gmail.com>
2021-05-25 17:01:42 -0400

    Update start-producer-console-replicated.sh

--- labs/lab1.2/start-producer-console-replicated.sh
+++ labs/lab1.2/start-producer-console-replicated.sh
@@ -3,7 +3,7 @@ cd ~/kafka-training
 
 
 ## TODO RUN THE KAFKA CONSOLE PRODUCER
-kafka/bin/kafka-console-producer.sh --bootstrap-server localhost:9094,localhost:9092 --topic my-failsafe-topic
+kafka/bin/kafka-console-producer.sh --broker-list localhost:9094,localhost:9092 --topic my-failsafe-topic
 
 ## TODO Specify the three Kafka servers that we ran earlier as bootstrap servers
 # HINT --bootstrap-server localhost:9094,localhost:9092 \
