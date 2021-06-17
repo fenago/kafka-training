37aef4a4305e17ecf6b09b9f300086024371a304
fenago <you@example.com>
2021-06-17 15:57:52 +0000

    scripts

--- labs/kafka-sandbox/install-mvn.sh
+++ labs/kafka-sandbox/install-mvn.sh
@@ -1,10 +1,12 @@
+
 #!/bin/sh
 
 yum remove -y maven || true
 
-export VER="3.6.1"
+export VER="3.8.1"
 
-curl -O http://www-eu.apache.org/dist/maven/maven-3/${VER}/binaries/apache-maven-${VER}-bin.tar.gz
+# curl -O http://www-eu.apache.org/dist/maven/maven-3/${VER}/binaries/apache-maven-${VER}-bin.tar.gz
+curl -O https://mirrors.gigenet.com/apache/maven/maven-3/${VER}/binaries/apache-maven-${VER}-bin.tar.gz
 
 tar xvf apache-maven-${VER}-bin.tar.gz
 
@@ -18,5 +20,14 @@ EOF
 
 source /etc/profile.d/maven.sh
 
+
+cat <<EOF | sudo tee -a ~/.bashrc
+source /etc/profile.d/maven.sh
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.11.0.9-1.el7_9.x86_64
+
+EOF
+
+
+source ~/.bashrc
 
 mvn --version
