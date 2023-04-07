29faf70988471f8882a4adea8fa9a204ae919188
athertahir <athertahir1995@gmail.com>
2023-04-07 18:18:57 +0500

    remove extra labs folder

--- run-schema_registry.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd ~/kafka-training
-
-# Already done
-# curl -O http://packages.confluent.io/archive/6.1/confluent-6.1.1.zip
-# unzip confluent-6.1.1.zip
-
-~/kafka-training/confluent-6.1.1/bin/schema-registry-start  ~/kafka-training/confluent-6.1.1/etc/schema-registry/schema-registry.properties
